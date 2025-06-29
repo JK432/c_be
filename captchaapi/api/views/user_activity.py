ae180711e49dd7f7ae199aa7edd79bdd49dbbae1
JK432 <jk04032002@gmail.com>
2025-06-30 03:48:08 +0530

    filter

--- captchaapi/api/views/user_activity.py
+++ captchaapi/api/views/user_activity.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets
 from api.models import UserActivity
 from api.serializers import UserActivitySerializer
+from django_filters.rest_framework import FilterSet
 
+
+class UserActivityFilter(FilterSet):
+    class Meta:
+        model = UserActivity
+        fields = {
+            'mode': settings.FILTER_NUMBER_MODELS,
+        }
 
 class UserActivityViewSet(viewsets.ModelViewSet):
     queryset = UserActivity.objects.all()
     serializer_class = UserActivitySerializer
+    filterset_class = UserActivityFilter
